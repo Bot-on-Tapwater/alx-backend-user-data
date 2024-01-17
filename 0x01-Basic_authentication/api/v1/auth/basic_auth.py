8991534bbdba22a04e652e09b0f2f6697478b137
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-17 08:56:56 +0300

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@
 from api.v1.auth.auth import Auth
 import base64
 import binascii
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -89,3 +91,31 @@ class BasicAuth(Auth):
             return (None, None)
         else:
             return tuple(decoded_base64_authorization_header.split(':'))
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        Generate a User object from the given credentials.
+
+        Args:
+            user_email (str): The email address of the user.
+            user_pwd (str): The password of the user.
+
+        Returns:
+            User: The User object if the credentials are valid, None otherwise.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        elif user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        current_user = User.search({'email': user_email})
+
+        if not current_user:
+            return None
+
+        elif not current_user[0].is_valid_password(user_pwd):
+            return None
+        else:
+            return current_user[0]
