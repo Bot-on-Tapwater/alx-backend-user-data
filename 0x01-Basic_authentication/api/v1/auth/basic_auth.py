4851644ed62b15fb6676d8e85ae761fd3f3aabb8
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-17 08:11:40 +0300

    Document extract_base64 function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,6 +8,26 @@ class BasicAuth(Auth):
     """BasicAuth class"""
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
+        """
+        Extracts the base64 authorization header
+        from the given authorization_header string.
+
+        Args:
+            self: The instance of the class.
+            authorization_header (str): The authorization header string.
+
+        Returns:
+            str: The extracted base64 authorization header.
+
+        Raises:
+            None.
+
+        Examples:
+            extract_base64_authorization_header(None, "") -> None
+            extract_base64_authorization_header(None, "Invalid Header") -> None
+            extract_base64_authorization_header
+            (None, "Basic ABC123") -> "ABC123"
+        """
         if authorization_header is None:
             return None
         elif not isinstance(authorization_header, str):
