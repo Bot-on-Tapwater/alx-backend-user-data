b0265d576622ed366ebae6f676033c4378ba0779
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-17 07:48:10 +0300

    Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -36,7 +36,13 @@ class Auth():
         Returns:
             str: The generated authorization header string.
         """
-        return None
+        if request is None:
+            return None
+
+        if 'Authorization' not in request.headers:
+            return None
+        else:
+            return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
