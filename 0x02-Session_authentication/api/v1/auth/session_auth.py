55676187cea668fe842131c7d2d0bb257378efd9
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-18 15:56:24 +0300

    User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -31,6 +31,23 @@ class SessionAuth(Auth):
         elif not isinstance(user_id, str):
             return None
         else:
-            session_id = uuid.uuid4()
+            session_id = str(uuid.uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns the user ID associated with the given session ID.
+
+        :param session_id: The session ID for which to retrieve the user ID.
+        :type session_id: str, optional
+        :return: The user ID associated with the session ID, or None if the
+        session ID is None or not a string.
+        :rtype: str or None
+        """
+        if session_id is None:
+            return None
+        elif not isinstance(session_id, str):
+            return None
+        else:
+            return self.user_id_by_session_id.get(session_id)
