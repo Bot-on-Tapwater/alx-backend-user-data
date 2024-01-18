ed9386eeff02e267309059026ec5bdd0e092710e
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-18 15:47:38 +0300

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,35 @@
 """Implement class SessionAuth"""
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Class SessionAuth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a new session for a user.
+
+        Args:
+            user_id (str): The ID of the user. Defaults to None.
+
+        Returns:
+            str: The session ID generated for the user.
+
+        Raises:
+            None
+
+        Notes:
+            - If the user_id is None, None is returned.
+            - If the user_id is not a string, None is returned.
+        """
+        if user_id is None:
+            return None
+        elif not isinstance(user_id, str):
+            return None
+        else:
+            session_id = uuid.uuid4()
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
