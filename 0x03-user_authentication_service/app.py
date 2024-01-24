e084a5b5b01eb32e2dc4a21349c116f35099bd30
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-24 12:13:07 +0300

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """Implement flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -16,5 +19,31 @@ def index() -> str:
     return jsonify(form)
 
 
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    """
+    Handle POST requests to create new users.
+
+    Retrieves email and password from the request form and attempts to
+    register a new user with the provided credentials.
+
+    Returns:
+        - If successful, it returns a JSON
+        response with the new user's email and a success message.
+        - If the email is already registered,
+        it returns a JSON response with an
+        error message and status code 400.
+    """
+    email = request.form['email']  # retrieve email from the request form
+    password = request.form['password']
+
+    try:
+        new_user = AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"})
+
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
