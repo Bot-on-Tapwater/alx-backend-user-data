1b9885ac781707aa2a0dcc0f16a2f5e58de72371
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-24 14:32:51 +0300

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Implement flask app"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -43,6 +43,32 @@ def users() -> str:
 
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    """
+    Route for logging in, takes email and password from
+    form data and validates the login.
+    If valid, creates a session and returns a JSON response with
+    the email and a success message,
+    also setting a session ID cookie. If not
+    valid, aborts with a 401 status code.
+    Returns a JSON response.
+    """
+    email = request.form['email']
+    password = request.form['password']
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+
+        response = jsonify({"email": f"{email}", "message": "logged in"})
+
+        response.set_cookie('session_id', session_id)
+
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
