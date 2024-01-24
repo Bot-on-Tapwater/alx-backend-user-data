a26a2395c4cceba6bf6bef70558a47ab165f5943
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-24 17:08:17 +0300

    Refactor code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -158,11 +158,12 @@ class Auth:
             raise ValueError()
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """Function to update password"""
+        """use reset token to find the user, and update their
+        password with this new one"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            password = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=password)
-            return None
-        except NoResultFound:
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, reset_token=None,
+                                 hashed_password=str(hashed_password))
+        except Exception:
             raise ValueError
