c5757c70fea87f2ed3a5cf8b5003b99d6119579c
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-24 14:48:02 +0300

    Find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -106,3 +106,12 @@ class Auth:
 
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, str]:
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
