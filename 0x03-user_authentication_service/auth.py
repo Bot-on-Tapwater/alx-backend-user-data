8176496c71db06a0c457ee4b1849c2245a2087ea
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-24 15:18:35 +0300

    Generate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -134,3 +134,27 @@ class Auth:
         self._db._session.commit()
 
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Retrieve the reset password token for a given email.
+
+        Args:
+            email (str): The email address of the user.
+
+        Returns:
+            str: The reset password token.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+
+            reset_token = _generate_uuid()
+
+            user.reset_token = reset_token
+
+            self._db.__session.commit()
+
+            return reset_token
+
+        except NoResultFound:
+            raise ValueError
